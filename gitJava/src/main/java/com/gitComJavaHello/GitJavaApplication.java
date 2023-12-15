fa467ce2bf59fc0be129a1e258fa919edbaaaa04
IsaiasKardoso <isaiaskardososilva@gmail.com>
2023-12-14 22:36:33 -0300

    Hello java no Github

---
+++ gitJava/src/main/java/com/gitComJavaHello/GitJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gitComJavaHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJavaApplication.class, args);
+	}
+
+}
---
+++ gitJava/src/test/java/com/gitComJavaHello/GitJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitComJavaHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
