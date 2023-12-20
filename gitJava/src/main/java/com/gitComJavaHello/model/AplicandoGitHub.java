1dd707f563aafead357afff4fb09721f290c948f
IsaiasKardoso <isaiaskardososilva@gmail.com>
2023-12-20 17:20:31 -0300

    Primeiro commit

---
+++ gitJava/src/main/java/com/gitComJavaHello/model/AplicandoGitHub.java
@@ -0,0 +1,8 @@
+package com.gitComJavaHello.model;
+
+public class AplicandoGitHub {
+    public static void main(String[] args) {
+        String string = "Óla mundo do Git";
+        System.out.println(string);
+    }
+}
